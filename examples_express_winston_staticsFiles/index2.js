6799860f86e2d42ec29fe362f48bcac7d9d9c6e0
Thalita Mariano <tmariano@eusou.space>
2020-06-01 08:25:15 -0300

    adding example's class api module

---
+++ examples_express_winston_staticsFiles/index2.js
@@ -0,0 +1,21 @@
+//import express from 'express'; don't work D:
+
+//Seção Middelwares - Nível do Roteador (test2/abc)
+
+// var express = require("express");
+
+// const app = express();
+// const router = express.Router();
+
+// router.get('/abc', (req, res) => {
+//   console.log('/test2');
+//   res.end();
+// });
+// //aqui jogamos o objeto inteiro no /test
+// app.use('/test2', router);
+
+// app.listen(3000, async () => {
+//   console.log('API started!');
+// });
+
+
--- exemplo03/scripts/script.js
+++
@@ -1,4 +0,0 @@
-console.log("Olá, mundo")
-
-var input1 = document.querySelector('#input1')
-input1.value = "banana caramelada com açúcar"
